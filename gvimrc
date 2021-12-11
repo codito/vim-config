24ef1a62f4a4d2c138b32a9405758ee82382e7e6
Arun Mahapatra <codito@codito.in>
2021-12-12 00:03:47 +0530

    feat: add nvim configurations.

--- gvimrc
+++ gvimrc
@@ -1,6 +1,6 @@
 " GVim config
 " Created: Aug 2005
-" Last Modified: 25/07/2021, 19:16:24 +0530
+" Last Modified: 11/12/2021, 08:02:20 +0530
 "
 
 " Window Appearance {{{1
@@ -8,7 +8,11 @@
 if GetPlatform() == "win"
     "set guifont=consolas:h9:cANSI
     "set guifont=JetBrains_Mono:h12:cANSI:qDRAFT
-    set guifont=Cascadia_Code:h12:cANSI:qDRAFT
+    if has("nvim")
+        set guifont=Cascadia\ Code:h12
+    else
+        set guifont=Cascadia_Code:h12:cANSI:qDRAFT
+    endif
     au GUIEnter * simalt ~x " maximize window on start
 else
     set guifont=JetBrains\ Mono\ 12
@@ -24,6 +28,12 @@ set mousehide		    " hide the mouse when typing text
 " Pure gvim {{{1
 "
 "set lines=80
+
+" NVim Qt {{{1
+if has("nvim")
+    " enable Shift+Insert
+    inoremap <silent>  <S-Insert>  <C-R>+
+endif
 
 " Key bindings {{{1
 "
---
+++ lua/config.lua
@@ -0,0 +1,178 @@
+-- NVIM lua config
+-- Created: 11/12/2021, 11:44:11 +0530
+-- Last modified: 11/12/2021, 19:57:47 +0530
+
+-- Lsp {{{1
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  -- Enable completion triggered by <c-x><c-o>
+  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  local opts = { noremap=true, silent=true }
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+end
+
+local lsp_installer = require("nvim-lsp-installer")
+local coq = require "coq"
+
+-- Register a handler that will be called for all installed servers.
+-- Alternatively, you may also register handlers on specific server instances instead (see example below).
+lsp_installer.on_server_ready(function(server)
+    local opts = {
+        on_attach = on_attach,
+        flags = {
+            debounce_text_changes = 150,
+        }
+    }
+
+    -- (optional) Customize the options passed to the server
+    -- if server.name == "tsserver" then
+    --     opts.root_dir = function() ... end
+    -- end
+
+    -- This setup() function is exactly the same as lspconfig's setup function.
+    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+    -- Decorate with coq to allow lsp completion.
+    server:setup(coq.lsp_ensure_capabilities(opts))
+end)
+
+-- Null LS {{{1
+-- https://github.com/jose-elias-alvarez/null-ls.nvim
+local null_ls = require("null-ls")
+null_ls.config({
+    sources = {
+        null_ls.builtins.diagnostics.proselint,     -- markdown
+        null_ls.builtins.diagnostics.vale,          -- markdown
+        null_ls.builtins.diagnostics.write_good,    -- markdown
+        null_ls.builtins.diagnostics.stylelint,     -- css
+        null_ls.builtins.diagnostics.flake8,        -- python
+
+        null_ls.builtins.formatting.clang_format.with({
+            filetypes = { "c", "cpp" }
+        }),
+        null_ls.builtins.formatting.eslint,
+        null_ls.builtins.formatting.prettier,
+
+        require("null-ls").builtins.formatting.stylua,
+        require("null-ls").builtins.completion.spell,
+    },
+    diagnostics_format = "#{m}",
+    debounce = 250,
+    default_timeout = 5000,
+    update_on_insert = false,
+    debug = false,
+})
+
+-- Tree sitter {{{1
+-- https://github.com/nvim-treesitter/nvim-treesitter
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn",
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+  indent = {
+    enable = true
+  },
+
+  -- Tree sitter context objects {{{2
+  -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  textobjects = {
+    select = {
+      enable = true,
+
+      -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
+    },
+
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>a"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>A"] = "@parameter.inner",
+      },
+    },
+
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
+
+    lsp_interop = {
+      enable = true,
+      border = 'none',
+      peek_definition_code = {
+        ["<leader>df"] = "@function.outer",
+        ["<leader>dF"] = "@class.outer",
+      },
+    },
+  },
+}
+
+-- Trouble {{{1
+-- https://github.com/folke/trouble.nvim
+require("trouble").setup {
+}
+
+-- vim: foldmethod=marker
+-- EOF
