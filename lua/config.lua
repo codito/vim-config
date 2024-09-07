97dd670ec907e3de56b11873db8c9e7eacc6624c
Arun Mahapatra <codito@codito.in>
2024-09-07 10:23:32 +0530

    fix: stylua integration and codecompanion handler for llama-server

--- lua/config.lua
+++ lua/config.lua
@@ -1,72 +1,77 @@
 -- NVIM lua config
 -- Created: 11/12/2021, 11:44:11 +0530
--- Last modified: 07/09/2024, 08:45:35 +0530
+-- Last modified: 07/09/2024, 10:22:33 +0530
 
 -- Include other configurations
 -- LLMs {{{1
-require('llm')
+require("llm")
 
 -- Aerial {{{1
 -- Symbols outliner for neovim
-require('aerial').setup({
+require("aerial").setup({
   -- optionally use on_attach to set keymaps when aerial has attached to a buffer
   on_attach = function(bufnr)
     -- Jump forwards/backwards with '{' and '}'
-    vim.keymap.set('n', '<leader>[', '<cmd>AerialPrev<CR>', {buffer = bufnr})
-    vim.keymap.set('n', '<leader>]', '<cmd>AerialNext<CR>', {buffer = bufnr})
-  end
+    vim.keymap.set("n", "<leader>[", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+    vim.keymap.set("n", "<leader>]", "<cmd>AerialNext<CR>", { buffer = bufnr })
+  end,
 })
 -- You probably also want to set a keymap to toggle aerial
-vim.keymap.set('n', '<leader>tt', '<cmd>AerialToggle! right<CR>')
+vim.keymap.set("n", "<leader>tt", "<cmd>AerialToggle! right<CR>")
 
 -- Comment {{{1
-require('Comment').setup()
+require("Comment").setup()
 
 -- Conform {{{1
 require("conform").setup({
-    formatters_by_ft = {
-        css = { "stylelint" },
-        -- lua = { "stylua" },
-        -- Conform will run multiple formatters sequentially
-        python = { "ruff_format" },
-        -- Use a sub-list to run only the first available formatter
-        astro = { "prettier" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        markdown = { "prettier" },
-    },
-    -- If this is set, Conform will run the formatter on save.
-    -- It will pass the table to conform.format().
-    -- This can also be a function that returns the table.
-    format_on_save = {
-      -- I recommend these options. See :help conform.format for details.
-      lsp_fallback = true,
-      timeout_ms = 500,
-    },
+  formatters_by_ft = {
+    css = { "stylelint" },
+    lua = { "stylua" },
+    -- Conform will run multiple formatters sequentially
+    python = { "ruff_format" },
+    -- Use a sub-list to run only the first available formatter
+    astro = { "prettier" },
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    markdown = { "prettier" },
+  },
+  -- If this is set, Conform will run the formatter on save.
+  -- It will pass the table to conform.format().
+  -- This can also be a function that returns the table.
+  format_on_save = {
+    -- I recommend these options. See :help conform.format for details.
+    lsp_fallback = true,
+    timeout_ms = 500,
+  },
 })
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = "*",
-    callback = function(args)
-        require("conform").format({ bufnr = args.buf })
-    end,
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
 })
 
 -- Coverage {{{1
 require("coverage").setup({
-    auto_reload = true
+  auto_reload = true,
 })
 
 -- Highlight colors {{{1
-require('nvim-highlight-colors').setup {}
+require("nvim-highlight-colors").setup({})
 
 -- Image {{{1
 -- https://github.com/3rd/image.nvim
-if vim.fn.GetPlatform() == "nix" and (os.getenv("KITTY_WINDOW_ID") ~= nil or os.getenv("TERM_PROGRAM") == "WezTerm") and vim.g.vscode == nil and not vim.g.neovide then
-    require("image").setup({})
+if
+  vim.fn.GetPlatform() == "nix"
+  and (os.getenv("KITTY_WINDOW_ID") ~= nil or os.getenv("TERM_PROGRAM") == "WezTerm")
+  and vim.g.vscode == nil
+  and not vim.g.neovide
+then
+  require("image").setup({})
 end
 
 -- Lazygit {{{1
-vim.keymap.set('n', '<leader>gg', '<cmd>LazyGit<CR>')
+vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>")
 
 -- Lsp {{{1
 -- Use an on_attach function to only map the following keys
@@ -79,31 +84,36 @@ local on_attach = function(client, bufnr)
   end
 end
 
-local cmp = require "cmp"
-local luasnip = require "luasnip"
+local cmp = require("cmp")
+local luasnip = require("luasnip")
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  return col ~= 0
+    and vim.api
+        .nvim_buf_get_lines(0, line - 1, line, true)[1]
+        :sub(col, col)
+        :match("%s")
+      == nil
 end
 cmp.setup({
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
+    { name = "nvim_lsp" },
     --{ name = 'vsnip' }, -- For vsnip users.
-    { name = 'luasnip' }, -- For luasnip users.
+    { name = "luasnip" }, -- For luasnip users.
     -- { name = 'ultisnips' }, -- For ultisnips users.
     --{ name = 'snippy' }, -- For snippy users.
-    { name = 'buffer' },
+    { name = "buffer" },
     {
       name = "dictionary",
       keyword_length = 2,
     },
-    { name = 'emoji' },
-    { name = 'path' },
+    { name = "emoji" },
+    { name = "path" },
   }),
   snippet = {
     expand = function(args)
-      require'luasnip'.lsp_expand(args.body)
-    end
+      require("luasnip").lsp_expand(args.body)
+    end,
   },
   mapping = cmp.mapping.preset.insert({
     ["<Tab>"] = cmp.mapping(function(fallback)
@@ -128,14 +138,14 @@ cmp.setup({
       end
     end, { "i", "s" }),
 
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ['<A-y>'] = require('minuet').make_cmp_map(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<A-y>"] = require("minuet").make_cmp_map(),
   }),
 })
 require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_snipmate").lazy_load()
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- Configure the installed lsp servers.
 -- lsp_installer setup must be called before lspconfig.
@@ -144,64 +154,64 @@ local lsp_installer = require("mason-lspconfig")
 lsp_installer.setup()
 
 local lspconfig = require("lspconfig")
-require('mason-lspconfig').setup({
-    ensure_installed = {
-        "astro",
-        "csharp_ls",
-        "cssls",
-        "html",
-        "jsonls",
-        "ltex",
-        "marksman",
-        "pyright",
-        "ruff_lsp",
-        "ts_ls",
-    },
-    handlers = {
-        function(server_name)
-            lspconfig[server_name].setup({
-                on_attach = on_attach,
-                capabilities = capabilities
-            })
-        end,
-    }
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "astro",
+    "csharp_ls",
+    "cssls",
+    "html",
+    "jsonls",
+    "ltex",
+    "marksman",
+    "pyright",
+    "ruff_lsp",
+    "ts_ls",
+  },
+  handlers = {
+    function(server_name)
+      lspconfig[server_name].setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+      })
+    end,
+  },
 })
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
   callback = function(ev)
     -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     -- local opts = { noremap=true, silent=true }
     local opts = { buffer = ev.buf }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set("n", "<space>wl", function()
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, opts)
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', '<space>f', function()
-      vim.lsp.buf.format { async = true }
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<space>f", function()
+      vim.lsp.buf.format({ async = true })
     end, opts)
   end,
 })
@@ -213,60 +223,94 @@ require("neotest").setup({
       dap = { justMyCode = false },
     }),
     require("neotest-dotnet"),
-    require("neotest-rust")
+    require("neotest-rust"),
   },
 })
 
-vim.keymap.set('n', '<leader>tr', '<cmd>lua require("neotest").run.run()<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>tf', '<cmd>lua require("neotest").run.run(vim.fn.expand("%"))<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>ta', '<cmd>lua require("neotest").run.run({suite = true})<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>tx', '<cmd>lua require("neotest").run.stop()<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>td', '<cmd>lua require("neotest").run.attach()<CR>', { noremap = true, silent = true })
+vim.keymap.set(
+  "n",
+  "<leader>tr",
+  '<cmd>lua require("neotest").run.run()<CR>',
+  { noremap = true, silent = true }
+)
+vim.keymap.set(
+  "n",
+  "<leader>tf",
+  '<cmd>lua require("neotest").run.run(vim.fn.expand("%"))<CR>',
+  { noremap = true, silent = true }
+)
+vim.keymap.set(
+  "n",
+  "<leader>ta",
+  '<cmd>lua require("neotest").run.run({suite = true})<CR>',
+  { noremap = true, silent = true }
+)
+vim.keymap.set(
+  "n",
+  "<leader>tx",
+  '<cmd>lua require("neotest").run.stop()<CR>',
+  { noremap = true, silent = true }
+)
+vim.keymap.set(
+  "n",
+  "<leader>td",
+  '<cmd>lua require("neotest").run.attach()<CR>',
+  { noremap = true, silent = true }
+)
 -- vim.keymap.set('n', '<leader>td', '<cmd>lua require("neotest").run.run({strategy = "dap"})<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>to', ':lua require("neotest").output.open({enter = true})<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>ts', ':lua require("neotest").summary.toggle()<CR>', { noremap = true, silent = true })
+vim.keymap.set(
+  "n",
+  "<leader>to",
+  ':lua require("neotest").output.open({enter = true})<CR>',
+  { noremap = true, silent = true }
+)
+vim.keymap.set(
+  "n",
+  "<leader>ts",
+  ':lua require("neotest").summary.toggle()<CR>',
+  { noremap = true, silent = true }
+)
 
 -- Nvim devicons {{{1
-require('nvim-web-devicons').setup({
-  default = true
+require("nvim-web-devicons").setup({
+  default = true,
 })
 
 -- Nvim tree {{{1
-require('nvim-tree').setup({
+require("nvim-tree").setup({
   update_cwd = false,
   filesystem_watchers = {
     enable = false,
     debounce_delay = 50,
     ignore_dirs = {
-      "node_modules"
+      "node_modules",
     },
   },
 })
-
 
 -- Telescope {{{1
 local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 vim.keymap.set("n", "<leader>/", builtin.live_grep, {})
 
-require('telescope').setup({
+require("telescope").setup({
   extensions = {
     file_browser = {
-      git_status = false
-    }
-  }
+      git_status = false,
+    },
+  },
 })
 
 -- Tree sitter {{{1
 -- https://github.com/nvim-treesitter/nvim-treesitter
-require'nvim-treesitter.configs'.setup {
+require("nvim-treesitter.configs").setup({
   highlight = {
     enable = true,
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = { 'markdown' },
+    additional_vim_regex_highlighting = { "markdown" },
   },
   incremental_selection = {
     enable = true,
@@ -278,7 +322,7 @@ require'nvim-treesitter.configs'.setup {
     },
   },
   indent = {
-    enable = true
+    enable = true,
   },
 
   -- Tree sitter context objects {{{2
@@ -330,52 +374,65 @@ require'nvim-treesitter.configs'.setup {
 
     lsp_interop = {
       enable = true,
-      border = 'none',
+      border = "none",
       peek_definition_code = {
         ["<leader>df"] = "@function.outer",
         ["<leader>dF"] = "@class.outer",
       },
     },
   },
-}
+})
 
 -- Thesaurus {{{1
-vim.cmd [[
+vim.cmd([[
 func Thesaur(findstart, base)
-  if a:findstart
-    return searchpos('\<', 'bnW', line('.'))[1] - 1
-  endif
-  let res = []
-  let h = ''
-  for l in systemlist('aiksaurus ' .. shellescape(a:base))
-    if l[:3] == '=== '
-      let h = '(' .. substitute(l[4:], ' =*$', ')', '')
-    elseif l ==# 'Alphabetically similar known words are: '
-      let h = "\U0001f52e"
-    elseif l[0] =~ '\a' || (h ==# "\U0001f52e" && l[0] ==# "\t")
-      call extend(res, map(split(substitute(l, '^\t', '', ''), ', '), {_, val -> {'word': val, 'menu': h}}))
-    endif
-  endfor
-  return res
+if a:findstart
+return searchpos('\<', 'bnW', line('.'))[1] - 1
+endif
+let res = []
+let h = ''
+for l in systemlist('aiksaurus ' .. shellescape(a:base))
+if l[:3] == '=== '
+let h = '(' .. substitute(l[4:], ' =*$', ')', '')
+elseif l ==# 'Alphabetically similar known words are: '
+let h = "\U0001f52e"
+elseif l[0] =~ '\a' || (h ==# "\U0001f52e" && l[0] ==# "\t")
+call extend(res, map(split(substitute(l, '^\t', '', ''), ', '), {_, val -> {'word': val, 'menu': h}}))
+endif
+endfor
+return res
 endfunc
-]]
+]])
 vim.opt.thesaurusfunc = "Thesaur"
 
 function ShowSynonymsForWordUnderCursor()
   local word = vim.fn.expand("<cword>")
   vim.cmd("Thesaurus " .. word)
 end
-vim.keymap.set('n', '<leader>ct', ShowSynonymsForWordUnderCursor, { noremap = true })
+
+vim.keymap.set(
+  "n",
+  "<leader>ct",
+  ShowSynonymsForWordUnderCursor,
+  { noremap = true }
+)
 
 -- Trouble {{{1
 -- https://github.com/folke/trouble.nvim
-require("trouble").setup {
-}
-vim.keymap.set('n', '<leader>xx', '<cmd>Trouble diagnostics toggle<cr>')
-vim.keymap.set('n', '<leader>xd', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>')
-vim.keymap.set('n', '<leader>xq', '<cmd>Trouble qflist toggle<cr>')
-vim.keymap.set('n', '<leader>xl', '<cmd>Trouble loclist toggle<cr>')
-vim.keymap.set('n', 'gR', '<cmd>Trouble lsp toggle focus=false win.position=right<cr>')
+require("trouble").setup({})
+vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>")
+vim.keymap.set(
+  "n",
+  "<leader>xd",
+  "<cmd>Trouble diagnostics toggle filter.buf=0<cr>"
+)
+vim.keymap.set("n", "<leader>xq", "<cmd>Trouble qflist toggle<cr>")
+vim.keymap.set("n", "<leader>xl", "<cmd>Trouble loclist toggle<cr>")
+vim.keymap.set(
+  "n",
+  "gR",
+  "<cmd>Trouble lsp toggle focus=false win.position=right<cr>"
+)
 
 -- vim: foldmethod=marker foldmarker={{{,}}}
 -- EOF
--- lua/llm.lua
+++ lua/llm.lua
@@ -1,73 +1,153 @@
 -- LLM plugins
 -- Created: 01/09/2024, 10:26:27 +0530
--- Last modified: 03/09/2024, 21:14:26 +0530
+-- Last modified: 07/09/2024, 10:23:11 +0530
 
 -- CodeCompanion {{{1
 require("codecompanion").setup({
-    adapters = {
-        openai = function()
-            return require("codecompanion.adapters").extend("openai", {
-                url = "http://localhost:8080/v1/chat/completions",
-                schema = {
-                    temperature = { default = 0.0 },
-                    max_tokens = { default = 512 }
-                }
-            })
-        end,
+  opt = {
+    -- log_level = "TRACE"
+  },
+  adapters = {
+    localai = function()
+      return require("codecompanion.adapters").extend("openai", {
+        url = "http://localhost:8000/v1/chat/completions",
+        schema = {
+          temperature = { default = 0.0 },
+          max_tokens = { default = 512 },
+        },
+        handlers = {
+          chat_output = function(data)
+            local output = {}
+
+            if data and data ~= "" then
+              local data_mod = data:sub(7)
+              local ok, json =
+                pcall(vim.json.decode, data_mod, { luanil = { object = true } })
+
+              if ok then
+                if #json.choices > 0 then
+                  local delta = json.choices[1].delta
+
+                  if delta.content then
+                    output.content = delta.content
+                    output.role = delta.role or "assistant" -- llama-server doesn't return role on streaming
+
+                    return {
+                      status = "success",
+                      output = output,
+                    }
+                  end
+                end
+              end
+            end
+          end,
+        },
+      })
+    end,
+  },
+  strategies = {
+    chat = {
+      adapter = "localai",
     },
+    inline = { adapter = "localai" },
+    agent = { adapter = "localai" },
+  },
+  display = {
+    chat = {
+      window = {
+        layout = "vertical", -- float|vertical|horizontal|buffer
+      },
+    },
+    inline = {
+      diff = {
+        hl_groups = {
+          added = "DiffAdd",
+        },
+      },
+    },
+  },
+  opts = {
+    force_role = true,
+    log_level = "TRACE",
+  },
 })
 
-vim.api.nvim_set_keymap("n", "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<C-a>", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<LocalLeader>co", "<cmd>CodeCompanionToggle<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "<LocalLeader>co", "<cmd>CodeCompanionToggle<cr>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "ga", "<cmd>CodeCompanionAdd<cr>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  "n",
+  "<C-a>",
+  "<cmd>CodeCompanionActions<cr>",
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "v",
+  "<C-a>",
+  "<cmd>CodeCompanionActions<cr>",
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<LocalLeader>co",
+  "<cmd>CodeCompanionToggle<cr>",
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "v",
+  "<LocalLeader>co",
+  "<cmd>CodeCompanionToggle<cr>",
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  "v",
+  "ga",
+  "<cmd>CodeCompanionAdd<cr>",
+  { noremap = true, silent = true }
+)
 
 -- Expand 'cc' into 'CodeCompanion' in the command line
 vim.cmd([[cab cc CodeCompanion]])
 
 -- Minuet {{{1
-require('minuet').setup({
-    provider = "openai_compatible",
-    context_window = 4000, -- chars, ~3 char = ~1 token
-    request_timeout = 3, -- in seconds
-    n_completions = 1,
-    provider_options = {
-        openai_compatible = {
-            model = 'llama-3.1-70b-versatile',
-            system = default_system,
-            few_shots = default_few_shots,
-            end_point = 'https://api.groq.com/openai/v1/chat/completions',
-            api_key = 'GROQ_API_KEY',
-            name = 'Groq',
-            stream = true,
-            optional = {
-                stop = nil,
-                max_tokens = 256,
-            },
-        },
-        -- openai_compatible = {
-        --     end_point = "http://localhost:8080/v1/chat/completions",
-        --     api_key = "SHELL", -- dummy key, must be a valid environment variable
-        --     name = "local",
-        --     stream = false,
-        --     optional = {
-        --         stop = nil,
-        --         max_tokens = 256,
-        --     },
-        -- },
-        -- openai_fim_compatible = {
-        --     end_point = "http://localhost:8080/completion",
-        --     api_key = "SHELL", -- dummy key, must be a valid environment variable
-        --     name = "local-fim",
-        --     stream = false,
-        --     optional = {
-        --         stop = { '\n\n' },
-        --         max_tokens = 256,
-        --     },
-        -- }
-    }
+require("minuet").setup({
+  provider = "openai_compatible",
+  context_window = 4000, -- chars, ~3 char = ~1 token
+  request_timeout = 3, -- in seconds
+  n_completions = 1,
+  provider_options = {
+    openai_compatible = {
+      model = "llama-3.1-70b-versatile",
+      system = default_system,
+      few_shots = default_few_shots,
+      end_point = "https://api.groq.com/openai/v1/chat/completions",
+      api_key = "GROQ_API_KEY",
+      name = "Groq",
+      stream = true,
+      optional = {
+        stop = nil,
+        max_tokens = 256,
+      },
+    },
+    -- openai_compatible = {
+    --     end_point = "http://localhost:8080/v1/chat/completions",
+    --     api_key = "SHELL", -- dummy key, must be a valid environment variable
+    --     name = "local",
+    --     stream = false,
+    --     optional = {
+    --         stop = nil,
+    --         max_tokens = 256,
+    --     },
+    -- },
+    -- openai_fim_compatible = {
+    --     end_point = "http://localhost:8080/completion",
+    --     api_key = "SHELL", -- dummy key, must be a valid environment variable
+    --     name = "local-fim",
+    --     stream = false,
+    --     optional = {
+    --         stop = { '\n\n' },
+    --         max_tokens = 256,
+    --     },
+    -- }
+  },
 })
 
--- vim: foldmethod=marker foldmarker={{{,}}}
+-- vim: foldmethod=marker foldmarker={{{,}}} ts=2 sw=2 et
 -- EOF
