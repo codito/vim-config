c228018a96c3ecdda9d366d0be3f725f1ee51efa
codito <codito@codito.in>
2024-09-17 07:07:15 +0530

    fix: disable animations for neovide on win

---
+++ lua/ui.lua
@@ -0,0 +1,14 @@
+-- Neovide settings
+
+if vim.fn.has("win32") or vim.fn.has("win64") then
+  if vim.g.neovide then
+    -- disable animations
+    vim.g.neovide_position_animation_length = 0
+    vim.g.neovide_cursor_animation_length = 0.00
+    vim.g.neovide_cursor_trail_size = 0
+    vim.g.neovide_cursor_animate_in_insert_mode = false
+    vim.g.neovide_cursor_animate_command_line = false
+    vim.g.neovide_scroll_animation_far_lines = 0
+    vim.g.neovide_scroll_animation_length = 0.00
+  end
+end
